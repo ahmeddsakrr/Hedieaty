2c010754f1df5583042d757e6618cd0e90374bfc
ahmeddsakrr <91761423+ahmeddsakrr@users.noreply.github.com>
2024-11-04 14:59:13 +0200

    Switched to SQLite File DB

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -6,12 +6,24 @@ import 'models/friend_table.dart';
 import 'models/gift_table.dart';
 import 'models/pledge_table.dart';
 import 'models/notification_table.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'dart:io';
 
 part 'app_database.g.dart';
 
 @DriftDatabase(tables: [Users, Friends, Events, Gifts, Pledges, Notifications])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase() : super(NativeDatabase.memory());
+  AppDatabase() : super(_openConnection());
 
+  @override
   int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'app_database.sqlite'));
+    return NativeDatabase(file);
+  });
 }
