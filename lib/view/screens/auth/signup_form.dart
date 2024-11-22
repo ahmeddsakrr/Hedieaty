e372d438e928c30aad1e7a4417da812cd23e8dc3
ahmeddsakrr <91761423+ahmeddsakrr@users.noreply.github.com>
2024-11-22 19:13:09 +0200

    Added validation for empty and null values for auth screen

--- lib/view/screens/auth/signup_form.dart
+++ lib/view/screens/auth/signup_form.dart
@@ -78,8 +78,6 @@ class _SignupFormState extends State<SignupForm> {
             validator: (value) {
               if (value == null || value.isEmpty) {
                 return 'Please enter your password';
-              } else if (value.length < 6) {
-                return 'Password must be at least 6 characters long';
               }
               return null;
             },
