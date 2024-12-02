7d8e2928d21641282ffd8f25bb2aa9dc59d06869
ahmeddsakrr <91761423+ahmeddsakrr@users.noreply.github.com>
2024-12-02 22:05:17 +0200

    Authentication Service

--- lib/controller/services/auth_service.dart
+++ lib/controller/services/auth_service.dart
@@ -1,37 +1,61 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:hedieaty/data/repositories/user_repository.dart';
+import '../../data/remote/firebase/models/user.dart' as RemoteUser;
+
+import '../../data/local/database/app_database.dart' as AppDatabase;
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final UserRepository _userRepository;
 
-  Future<User?> signUpWithEmailAndPassword(String email, String password) async {
-    try {
-      UserCredential result = await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return result.user;
-    } catch (e) {
-      if (kDebugMode) {
-        print(e.toString());
+  AuthService(AppDatabase.AppDatabase db) : _userRepository = UserRepository(db);
+
+  Future<User?> signup(String name, String email, String phoneNumber, String password) async {
+    RemoteUser.User? user = await _userRepository.getUserByPhoneNumber(phoneNumber);
+    if (user == null) {
+      try {
+        UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        User? firebaseUser = result.user;
+        if (firebaseUser != null) {
+          await _userRepository.addUser(RemoteUser.User(
+            name: name,
+            email: email,
+            phoneNumber: phoneNumber,
+            password: password,
+          ));
+        }
+        return firebaseUser;
+      } catch (e) {
+        if (kDebugMode) {
+          print(e.toString());
+        }
+        return null;
       }
-      return null;
     }
+    return null;
   }
 
-  Future<User?> signInWithEmailAndPassword(String email, String password) async {
-    try {
-      UserCredential result = await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return result.user;
-    } catch (e) {
-      if (kDebugMode) {
-        print(e.toString());
+  Future<User?> login(String phoneNumber, String password) async {
+    RemoteUser.User? user = await _userRepository.getUserByPhoneNumber(phoneNumber);
+    if (user != null) {
+      try {
+        UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: user.email,
+          password: password,
+        );
+        return result.user;
+      } catch (e) {
+        if (kDebugMode) {
+          print(e.toString());
+        }
+        return null;
       }
-      return null;
     }
+    return null;
   }
 
   User? getCurrentUser() {
