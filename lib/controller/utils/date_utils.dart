a7ad506610963fd4dfa533090b08c33303ac099a
ahmeddsakrr <91761423+ahmeddsakrr@users.noreply.github.com>
2024-12-02 18:48:50 +0200

    Added utility to parse dates

--- lib/controller/utils/date_utils.dart
+++ lib/controller/utils/date_utils.dart
@@ -3,3 +3,7 @@ import 'package:intl/intl.dart';
 String getFormattedDate(DateTime date) {
   return DateFormat.yMMMd().format(date);
 }
+
+DateTime parseFormattedDate(String dateStr) {
+return DateFormat.yMMMd().parse(dateStr);
+}
